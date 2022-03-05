1ef16c34fa01eaaad3ff011e30cba891af8b5b18
DillonRampersad <dillonrampersad2000@gmail.com>
2022-03-05 18:24:41 -0400

    attempting chart

--- Frontend/src/app/pages/charts/charts.component.ts
+++ Frontend/src/app/pages/charts/charts.component.ts
@@ -5,73 +5,72 @@ import { map } from 'rxjs';
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.css']
+  styleUrls: ['./charts.component.css'],
 })
 export class ChartsComponent implements OnInit {
+  chart = [];
+  constructor(private chartService: ChartsService) {}
 
-  chart=[];
-  constructor(private chartService : ChartsService) { }
-
-  
   ngOnInit(): void {
-    this.chartService.noOfEqu().subscribe(res=>{
-      let areaCode = res.map(res=>res.areaCode)
-      let inoperEqu = res.map(res=>res.inoperEqu)
-      let operEqu = res.map(res=>res.operEqu)
-      let date = res.map(res=>res.eventDate)
-      
-      let Dates = []
-      date.forEach((res) => {
-        let jsdate = new Date(res)
-        Dates.push(jsdate.toLocaleTimeString('en', {year: 'numeric', month: 'short', day:'numeric' }))
-        
-      });
-      console.log(operEqu)
-      console.log(Dates)
-      console.log(areaCode)
-      console.log(inoperEqu)
+    this.chartService.noOfEqu().subscribe((res) => {
+      let areaCode = res.map((res) => res.areaCode);
+      let inoperEqu = res.map((res) => res.inoperEqu);
+      let operEqu = res.map((res) => res.operEqu);
+      let date = res.map((res) => res.eventDate);
 
-/*
-      const chart = new Chart('canvas',{
+      let Dates = [];
+      date.forEach((res) => {
+        let jsdate = new Date(res);
+        Dates.push(
+          jsdate.toLocaleTimeString('en', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          })
+        );
+      });
+      console.log(operEqu);
+      console.log(Dates);
+      console.log(areaCode);
+      console.log(inoperEqu);
+
+      new Chart('canvas', {
         type: 'line',
-          data: {
-            labels: areaCode,
-            datasets: [
-              {
-                data: inoperEqu,
-                borderColor: '#3cba9f',
-                fill: false
-              },
-              {
-                data: operEqu,
-                borderColor: '#ffcc00',
-                fill: false
-              },
-            ]
+        data: {
+          labels: [],
+          datasets: [
+            {
+              data: areaCode,
+              borderColor: '#3cba9f',
+              fill: false,
+            },
+            {
+              data: operEqu,
+              borderColor: '#ffcc00',
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            },
           },
-          options: {
-                legend: {
-                  display: false
-                },
-                scales: {
-                  xAxes: [
-                    {
-                      ticks: {
-                        display: true
-                       }
-                    }
-                  ],
-                  yAxes: [
-                    {
-                      ticks: {
-                        display: true
-                       }
-                    }
-                  ]
-                 }
-              }
-        })*/
-      })
-      
+          scales: {
+            x: {
+              ticks: {
+                display: true,
+              },
+            },
+            y: {
+              ticks: {
+                display: true,
+              },
+            },
+          },
+        },
+      });
+    });
   }
 }
